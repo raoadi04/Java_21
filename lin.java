1fa924c7c7b1e26c7f32ad0da286f022a276a9c6
Aditya Rao <44147589+raoadi04@users.noreply.github.com>
2024-08-07 11:44:34 -0400

    Update lin.java
    formatting the class name to match the file name

--- lin.java
+++ lin.java
@@ -1,5 +1,5 @@
 import java.awt.Graphics;
-public class MyClass extended java.applet.Applet {
+public class lin extended java.applet.Applet {
   public void paint(Graphics g) 
   {
     g.drawLine(25,25,75,75);
