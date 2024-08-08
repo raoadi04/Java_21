075a8dd26c88b803212501056619fa9507c2cff3
Aditya Rao <44147589+raoadi04@users.noreply.github.com>
2024-08-08 13:11:49 -0400

    Update date_app.java
    formatting the code

--- date_app.java
+++ date_app.java
@@ -1,23 +1,23 @@
-1: import java.awt.Graphics;
-2: import java.awt.Font;
-3: import java.util.Date;
-4:
-5: public class DigitalClock extends java.applet.Applet {
-6:
-7: Font theFont = new Font(“TimesRoman”,Font.BOLD,24);
-8: Date theDate;
-9:
-10: public void start() {
-11: while (true) {
-12: theDate = new Date();
-13: repaint();
-14: try { Thread.sleep(1000); }
-15: catch (InterruptedException e) { }
-16: }
-17: }
-18:
-19: public void paint(Graphics g) {
-20: g.setFont(theFont);
-21: g.drawString(theDate.toString(),10,50);
-22: }
-23: }
+import java.awt.Graphics;
+import java.awt.Font;
+import java.util.Date;
+
+public class DigitalClock extends java.applet.Applet {
+
+Font theFont = new Font(“TimesRoman”,Font.BOLD,24);
+Date theDate;
+
+ public void start() {
+ while (true) {
+ theDate = new Date();
+ repaint();
+ try { Thread.sleep(1000); }
+ catch (InterruptedException e) { }
+ }
+ }
+
+ public void paint(Graphics g) {
+ g.setFont(theFont);
+ g.drawString(theDate.toString(),10,50);
+ }
+ }
